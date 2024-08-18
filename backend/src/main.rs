45e1507c90169cf96191573e02f8df131163f672
Lucas Pedro da Hora <lucaspedro517@gmail.com>
2024-08-18 01:17:56 -0300

    ✨Feat: Adding tests to backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    extract::Json,
+    response::Html,
+    routing::{get, post},
+    Router,
+};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/users", post(get_user_by_email));
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    "<h1>Hello, World 2!</h1>".into()
+}
+
+#[derive(Deserialize, Serialize)]
+struct CreateUserPayload {
+    email: String,
+    password: String,
+}
+#[derive(Deserialize, Serialize)]
+struct ResponseCreateUserPayload {
+    email: String,
+    password: String,
+    invoice: bool,
+}
+
+async fn get_user_by_email(
+    Json(payload): Json<CreateUserPayload>,
+) -> Json<ResponseCreateUserPayload> {
+    let payload_with_boolean = ResponseCreateUserPayload {
+        email: payload.email,
+        password: payload.password,
+        invoice: true,
+    };
+
+    payload_with_boolean.into()
 }
