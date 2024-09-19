78c655717ba7d4899df2241cb8fccb3e20e875d4
Lucas Pedro da Hora <lucaspedro517@gmail.com>
2024-09-18 23:29:16 -0300

    ✨Feat: Removing backend old project.

--- backend/src/utils.rs
+++
@@ -1,6 +0,0 @@
-pub fn is_valid_email(email: &str) -> bool {
-    const EMAIL_REGEX: &str = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
-
-    let regex: regex::Regex = regex::Regex::new(EMAIL_REGEX).unwrap();
-    regex.is_match(email)
-}
